72da838d8ac8ddfd38d06147664adaf3d670c03e
Alexandre Sousa <alexandresousaroberto@gmail.com>
2025-10-24 17:43:07 -0300

    corrigindo dirname para o node acessar as rotas

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Corrige __dirname (em ES Modules não existe por padrão)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
