fdf9287996564de8b80ee23661cacb808654e22e
Alexandre Sousa <alexandresousaroberto@gmail.com>
2026-09-07 19:58:57 -0300

    adiciona PWA e logo Dougão Lanches

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CartProvider } from "./context/CartContext";
+import ServiceWorkerRegister from "./components/ServiceWorkerRegister";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,9 +16,13 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Dougão Lanches",
-  description: "Cardápio digital",
+  description: "Cardápio digital Dougão Lanches",
+  manifest: "/manifest.webmanifest",
+  icons: {
+    icon: "/icons/icon-192.png",
+    apple: "/icons/icon-192.png",
+  },
 };
-
 export default function RootLayout({
   children,
 }: {
@@ -26,6 +31,8 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ServiceWorkerRegister />
+
         <CartProvider>
           {children}
         </CartProvider>
